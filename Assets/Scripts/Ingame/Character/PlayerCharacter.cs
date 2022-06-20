d134349391c7da78b2ffcd7a3f34c04409182848
jj139811 <jj139811@kaist.ac.kr>
2022-06-20 21:12:35 +0900

    Initial commit with basic collision physics

---
+++ Assets/Scripts/Ingame/Character/PlayerCharacter.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ingame.Character
+{
+    public class PlayerCharacter: MonoBehaviour
+    {
+        public float moveSpeed {get; private set;} = 10;
+        private void Awake() {
+            
+        }
+    }
+}
